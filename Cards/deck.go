a99168b83bce6fc34378827e54706301a0411f7d
rahul_bhataniya <rahulbhataniya2468@gmail.com>
2023-10-19 09:10:12 +0530

    multiple return values from one function

--- Cards/deck.go
+++ Cards/deck.go
@@ -6,8 +6,17 @@ import "fmt"
 
 type deck []string
 
+func newCard() deck {
+	cards := []string{"one", "two", "three","four","five"}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck,handsize int) (deck,deck){
+	return d[:handsize],d[handsize:]
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,11 +1,8 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Diamond"}
-	cards = append(cards, "new value")
-	cards.print()
-}
-
-func newCard() string {
-	return "value returned from function"
+	cards := newCard()
+	hand, remainingCards := deal(cards, 3)
+	hand.print()
+	remainingCards.print()
 }
