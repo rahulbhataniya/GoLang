f7b344ff2c48585a0bae2c325a61480bab965b48
rahul_bhataniya <rahulbhataniya2468@gmail.com>
2023-10-22 16:17:52 +0530

    Test Save To File And New Deck From File

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,10 +1,28 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 16 {
 		t.Errorf("Expected deck lenght of 16 but we got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spade but got %s", d[0])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 16 {
+		t.Errorf(" Expected length 16 but got %d", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
